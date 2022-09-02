bc2b9c50cc4e489f269c8a6722a1904fc4f3bea6
shafiq-ahmed <shafiqahmd3@gmail.com>
2022-09-02 16:42:14 +0600

    Basic structure created

---
+++ src/com/company/workspaceit/task2/Bank.java
@@ -0,0 +1,9 @@
+package com.company.workspaceit.task2;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Bank {
+    private int totalUsers;
+    private static List<User> users=new ArrayList<User>(5);
+}
---
+++ src/com/company/workspaceit/task2/User.java
@@ -0,0 +1,11 @@
+package com.company.workspaceit.task2;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class User {
+    private int id;
+    private String name;
+    private int balance;
+    private List<String> transactionHistory= new ArrayList<>(3);
+}
