c2d4f42e7cb7e5b090294c96ae4cdeb634c646af
shafiq-ahmed <shafiqahmd3@gmail.com>
2022-09-03 21:48:05 +0600

    exception message changed

--- src/com/company/workspaceit/task2/InsufficientFundsException.java
+++ src/com/company/workspaceit/task2/InsufficientFundsException.java
@@ -2,6 +2,6 @@ package com.company.workspaceit.task2;
 
 public class InsufficientFundsException extends Exception{
     public InsufficientFundsException(){
-        super("Insufficient funds in account for withdrawal");
+        super("Insufficient funds. Tk.500 must remain after withdrawal");
     }
 }
